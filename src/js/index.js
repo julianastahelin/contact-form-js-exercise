881fddf8f2efb05ae6db80490c4628005e47132f
Juliana Stahelin <julianastahelin@gmail.com>
2023-03-20 16:23:05 -0300

    corrections in submit button

--- src/js/index.js
+++ src/js/index.js
@@ -1,17 +1,28 @@
-const mandatoryInputs = document.querySelectorAll('.mandatory-input');
+let mandatoryInputs = document.querySelectorAll('.mandatory-input');
+let telephone = document.querySelector('#telephone');
 let mandatoryMessage = document.querySelectorAll('.mandatory-message');
 const submitBtn = document.querySelector('.submit');
 
-submitBtn.addEventListener('click', function () {
+submitBtn.addEventListener('click', function (event) {
     for (let i = 0; i < mandatoryInputs.length; i++) {
         if (mandatoryInputs[i].value === "") {
             mandatoryInputs[i].classList.add('alert');
             mandatoryMessage[i].classList.add('alert');
-        } 
+            event.preventDefault();
+        }
         if (mandatoryInputs[i].value !== "") {
             mandatoryInputs[i].classList.remove('alert');
             mandatoryMessage[i].classList.remove('alert');
         }
+    }
+    if (telephone.value.length < 10) {
+        telephone.classList.remove('green');
+        telephone.classList.add('alert');
+        alert('Insira um número de telefone válido');
+        event.preventDefault();
+    } else if (telephone.value.length >= 10 && telephone.value.length <= 13) {
+        telephone.classList.remove('alert');    
+        telephone.classList.add('green');
     }
 }
 )
@@ -27,4 +38,5 @@ for (let i = 0; i < mandatoryInputs.length; i++) {
             mandatoryMessage[i].classList.remove('alert');
         }
     })
+
 }
